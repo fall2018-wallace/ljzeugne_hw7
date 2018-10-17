52d50778057e3c92bb5c40188b2e6853afa85655
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 14:05:17 -0400

    Saving Visualizations.R

--- Visualizations.R
+++ Visualizations.R
@@ -22,8 +22,7 @@ map.murder <- map.murder + geom_map(map = us, aes(fill = Murder))
 map.murder <- map.murder + expand_limits(x = us$long, y= us$lat)
 map.murder <- map.murder + coord_map() + ggtitle("State Murderrate")
 
-bad <- dfMerged[dfMerged$center$x < -125, ]
-bad$state
+
 dfMerged <- dfMerged[dfMerged$center$x > -125, ]
 dfMerged$center$x
 
