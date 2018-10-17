5d7cec8270cbb6982e4651d671672238b30ca33e
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 13:49:37 -0400

    Saving Visualizations.R

--- Visualizations.R
+++ Visualizations.R
@@ -25,4 +25,5 @@ map.murder <- map.murder + coord_map() + ggtitle("State Murderrate")
 bad <- dfMerged[dfMerged$center$x < -125, ]
 bad$state
 dfMerged <- dfMerged[dfMered$center$x > 125,]
+
 map.popCircle <- map.popColor + geom_point(aes(x =center$x, y = center$y))
