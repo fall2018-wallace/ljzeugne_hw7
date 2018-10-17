37fa9c8e595a1c6f28790eb41896548933bf0c1d
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 13:15:13 -0400

    Saving Visualizations.R

--- Visualizations.R
+++ Visualizations.R
@@ -13,5 +13,5 @@ map.simple <- map.simple + coord_map() + ggtitle("Basic Map of continental USA")
 
 map.popColor <- ggplot(dfMerged, aes(map_id = state))
 map.popColor <- map.popColor + geom_map(map = us, aes(fill = population))
-map.popColor <- map.popColor + expand_limits (x = us$lat)
+map.popColor <- map.popColor + expand_limits(x = us$long, y= us$lat)
 map.popColor <- map.popColor + map.popColor + coord_map() + ggtitle("State Population")
