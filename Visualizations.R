29f7742383aac376b490d87b4ce1662db304517b
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 13:35:58 -0400

    Saving Visualizations.R

--- Visualizations.R
+++ Visualizations.R
@@ -21,4 +21,4 @@ map.murder <- map.murder + geom_map(map = us, aes(fill = Murder))
 map.murder <- map.murder + expand_limits(x = us$long, y= us$lat)
 map.murder <- map.murder + coord_map() + ggtitle("State Murderrate")
 
-map.popCircle <- map.popColor + geom_point(aes(x =-100, y = 30))
+map.popCircle <- map.popColor + geom_point(aes(x =center$x, y = center$y))
