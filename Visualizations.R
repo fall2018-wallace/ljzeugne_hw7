dcd2dfade316131f781cafc35b4dce5b18e2349e
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 13:24:30 -0400

    Saving Visualizations.R

--- Visualizations.R
+++ Visualizations.R
@@ -3,7 +3,6 @@ library(ggplot2)
 library(ggmap)
 us <- map_data("state")
 dfMerged$state <- tolower(state.name)
-dfMerged$area <- NULL
 str(dfMerged)
 
 map.simple <- ggplot(dfMerged, aes(map_id = state))
