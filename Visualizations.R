f19cbe17c55ed5dddce05e07d0111440bc203acd
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 13:55:47 -0400

    Saving Visualizations.R

--- Visualizations.R
+++ Visualizations.R
@@ -10,7 +10,12 @@ map.simple <- map.simple + geom_map(map = us, fill="white", color="black")
 map.simple <- map.simple + expand_limits(x=us$long, y =us$lat)
 map.simple <- map.simple + coord_map() + ggtitle("Basic Map of continental USA")
 
+
+bad <- dfMerged[dfMerged$center$x < -125, ]
+bad$state
 dfMerged <- dfMerged[dfMerged$center$x > -125, ]
+dfMerged$center$x
+
 map.popColor <- ggplot(dfMerged, aes(map_id = state))
 map.popColor <- map.popColor + geom_map(map = us, aes(fill = area))
 map.popColor <- map.popColor + expand_limits(x = us$long, y= us$lat)
@@ -22,9 +27,5 @@ map.murder <- map.murder + geom_map(map = us, aes(fill = Murder))
 map.murder <- map.murder + expand_limits(x = us$long, y= us$lat)
 map.murder <- map.murder + coord_map() + ggtitle("State Murderrate")
 
-bad <- dfMerged[dfMerged$center$x < -125, ]
-bad$state
-dfMerged <- dfMerged[dfMerged$center$x > -125, ]
-dfMerged$center$x
 
 map.popCircle <- map.popColor + geom_point(aes(x =dfMerged$center$x, y = dfMerged$center$y))
