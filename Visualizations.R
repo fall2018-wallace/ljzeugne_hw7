5be105ebefc2199ac9dea7ca83a4838e5fe7a9e0
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 21:14:09 -0400

    Saving Visualizations.R

--- Visualizations.R
+++ Visualizations.R
@@ -16,6 +16,7 @@ map.popColor <- ggplot(dfMerged, aes(map_id= state))
 map.popColor <- map.popColor + geom_map(map = us, aes(fill= population)
 map.popColor <- map.popColor + expand_limits(x=us$long, y=us$lat)
 map.popColor <- map.popColor + coord_map() + ggtitle("State Population")
+map.popColor
 
 
 
