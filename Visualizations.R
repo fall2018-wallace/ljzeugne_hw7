3aa3b6078e91b1863744dfcbac60843932f0eba9
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 21:46:34 -0400

    Saving Visualizations.R

--- Visualizations.R
+++ Visualizations.R
@@ -26,6 +26,5 @@ map.popMurder <- map.popMurder + expand_limits(x=us$long, y=us$lat)
 map.popMurder <- map.popMurder + coord_map() + ggtitle("State Murderrate")
 
 newMerged <- dfMerged[dfMerged$x > -125, ]
-newMerged$x
 
-map.popCircle <- map.simple + geom_point(data=dfMerged, aes(x=dfMerged$x, y=dfMerged$y), shape = 1)
+map.popCircle <- map.simple + geom_point(data=newMerged, aes(x=newMerged$x, y=newMerged$y), shape = 1)
