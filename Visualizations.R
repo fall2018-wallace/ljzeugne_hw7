660a5d3401b35f5c3da62ae8b9b642e606ad2753
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 22:57:35 -0400

    Saving Visualizations.R

--- Visualizations.R
+++ Visualizations.R
@@ -12,6 +12,7 @@ map.simple <- map.simple + geom_map(map = us, fill="white", color="black")
 map.simple <- map.simple + expand_limits(x=us$long, y =us$lat)
 map.simple <- map.simple + coord_map() + ggtitle("Basic Map of continental USA")
 
+"Step B"
 "Create a color coded map, based on the area of the state"
 map.popColor <- ggplot(dfMerged, aes(map_id= state))
 map.popColor <- map.popColor + geom_map(map = us, aes(fill= population))
